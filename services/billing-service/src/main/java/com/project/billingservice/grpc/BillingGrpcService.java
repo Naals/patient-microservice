b44c78a1f31656ab0a67774a90ba1911dc1db454
Aslan <tynyshtykaslan06@gmail.com>
2026-01-03 12:11:58 +0500

    dockerfiles error with grpc fixed

--- services/billing-service/src/main/java/com/project/billingservice/grpc/BillingGrpcService.java
+++ services/billing-service/src/main/java/com/project/billingservice/grpc/BillingGrpcService.java
@@ -2,14 +2,14 @@ package com.project.billingservice.grpc;
 
 import billing.BillingRequest;
 import billing.BillingResponse;
-import billing.BillingServiceGrpc.BillingServiceImplBase;
+import billing.BillingServiceGrpc;
 import io.grpc.stub.StreamObserver;
 import net.devh.boot.grpc.server.service.GrpcService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @GrpcService
-public class BillingGrpcService extends BillingServiceImplBase {
+public class BillingGrpcService extends BillingServiceGrpc.BillingServiceImplBase {
 
     private static final Logger log = LoggerFactory.getLogger(
             BillingGrpcService.class);
