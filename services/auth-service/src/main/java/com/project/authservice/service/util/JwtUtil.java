fa2bbb84619915796f2f110455fa8ab01c41eccf
Aslan <tynyshtykaslan06@gmail.com>
2026-01-03 21:43:03 +0500

    error fixed, wrong method was used

--- services/auth-service/src/main/java/com/project/authservice/service/util/JwtUtil.java
+++ services/auth-service/src/main/java/com/project/authservice/service/util/JwtUtil.java
@@ -3,14 +3,14 @@ package com.project.authservice.service.util;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
-
-import javax.crypto.SecretKey;
+import io.jsonwebtoken.security.SignatureException;
 import java.nio.charset.StandardCharsets;
 import java.security.Key;
 import java.util.Base64;
 import java.util.Date;
+import javax.crypto.SecretKey;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 @Component
 public class JwtUtil {
@@ -18,13 +18,10 @@ public class JwtUtil {
     private final Key secretKey;
 
     public JwtUtil(@Value("${jwt.secret}") String secret) {
-
         byte[] keyBytes = Base64.getDecoder()
                 .decode(secret.getBytes(StandardCharsets.UTF_8));
-
         this.secretKey = Keys.hmacShaKeyFor(keyBytes);
     }
-
 
     public String generateToken(String email, String role) {
         return Jwts.builder()
@@ -37,14 +34,14 @@ public class JwtUtil {
     }
 
     public void validateToken(String token) {
-        try{
-            Jwts.parser()
-                    .verifyWith((SecretKey) secretKey)
+        try {
+            Jwts.parser().verifyWith((SecretKey) secretKey)
                     .build()
-                    .parseEncryptedClaims(token);
+                    .parseSignedClaims(token);
+        } catch (SignatureException e) {
+            throw new JwtException("Invalid JWT signature");
         } catch (JwtException e) {
             throw new JwtException("Invalid JWT");
         }
     }
-
 }
